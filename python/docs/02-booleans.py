ec90abcd47513eb3e15158ebdbf0381e9224ff4e
Felipe <juanferocas@gmail.com>
2024-10-02 14:37:36 -0500

    DOCS

---
+++ python/docs/02-booleans.py
@@ -0,0 +1,10 @@
+"""Los booleanos son tipos de datos True o False, funcionan para tener conocimiento
+de los estados.
+"""
+
+print("hola".islower())
+print("HoLa".isupper())
+print("1299".isdigit())
+print("Python 3".isalpha())
+print("¡¡¡Python 3!!!".isalnum())
+
---
+++ python/docs/03-loops.py
@@ -0,0 +1,10 @@
+while CONDICIÓN:
+  # <----- el bloque debe estár correctamente indentado.
+  # ...
+  break
+  # El código a partir de acá no se ejecutará
+  # y se pasará a ejecutar el código justo después
+  # del bloque del ciclo.
+
+# El código encontrado a partir de acá será ejecutado justo
+# después de encontrar la sentencia break.
